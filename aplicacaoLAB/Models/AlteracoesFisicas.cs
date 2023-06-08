e816de7011c18a5fda5a63c375723a3ea3d261f4
luisbriga75897 <al75897@alunos.utad.pt>
2023-06-08 17:34:44 +0100

    adicionei as views e ViewModel

---
+++ aplicacaoLAB/Models/AlteracoesFisicas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using trabalhoLab.Classes;
+
+namespace trabalhoLab.Classes
+{
+    public class AlteracoesFisicas
+    {
+        public TipoRegistoFisico TipoRegistoFisico { get; set; } // tipo do registo das alterações físicas - TipoRegistoFisico - TipoRegistoFisico(541,"Peso","Kg")
+        public float Valor { get; set; } // valor do registo - flaot - 12.5
+        public DateTime Data { get; set; }    // data do registo - "12-03-2006"
+
+        public XElement ToXML()
+        {
+            XElement no = new XElement("alteracaoFisica");
+            no.Add(new XAttribute("valor", Valor));
+            no.Add(new XAttribute("data", Data.Date.ToShortDateString()));
+            no.Add(TipoRegistoFisico.ToXML());
+
+            return no;
+        }
+
+    }
+}
